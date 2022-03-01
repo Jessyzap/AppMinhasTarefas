7ea60f258d133a8d69a9b4a8a4dd5cc4df9e1f5a
Jessica Zaparolli <j.zaparolli.arq@gmail.com>
2022-03-01 19:36:07 -0300

    bottomsheet base behavior

---
+++ app/src/main/java/com/ctt/minhastarefas/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,24 @@
+package com.ctt.minhastarefas
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+open class BaseBottomSheetDialogFragment: BottomSheetDialogFragment() {
+
+    override fun onStart() {
+        super.onStart()
+        val sheetContainer = requireView().parent as? ViewGroup ?: return
+        sheetContainer.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireContext(), theme).apply {
+            behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+            behavior.halfExpandedRatio = 0.90F
+        }
+    }
+}
